cae8d3843c4dbdd80ab2ee12cd94ca98ac819d86
Susi Rathmann <susi@context.io>
2015-06-09 09:51:45 -0500

    update package name and set up for PyPI

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ README = open(os.path.join(here, 'README.md')).read()
 requires=['rauth', 'six']
 
 setup(name='contextio',
+    packages = ['contextio'],
     version='1.2',
     description='Library for accessing the Context.IO API v2.0 in Python',
     long_description=README,
     author='Tony Blank, Jesse Dhillon',
     author_email='tony@context.io, jesse@deva0.net',
     url='http://context.io',
-    keywords=['contextIO', 'dokdok', 'imap', 'oauth'],
+    keywords=['contextIO', 'imap', 'oauth'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
-    download_url='https://github.com/contextio/Python-ContextIO/tarball/0.1',
+    download_url='https://github.com/contextio/Python-ContextIO/tarball/1.2',
 )
