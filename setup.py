49a66fc66669b196d05b71e38d6e10155510d81e
Susi Rathmann <susi@context.io>
2015-06-09 10:30:18 -0500

    ready for release to PyPI

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ import os
 
 from setuptools import setup, find_packages
 
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
 requires=['rauth', 'six']
 
 setup(name='contextio',
