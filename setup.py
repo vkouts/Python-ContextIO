24218c4925be0b075d1c6944cc974a13973cab4c
Susi Rathmann <susi@context.io>
2015-06-17 16:30:48 -0500

    fix readme issue for pip installs

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-
 requires=['rauth', 'six']
 
 setup(name='contextio',
-    version='1.4',
+    version='1.5',
     description='Library for accessing the Context.IO API v2.0 in Python',
-    long_description=README,
     author='Tony Blank, Jesse Dhillon',
     author_email='tony@context.io, jesse@deva0.net',
     url='http://context.io',
@@ -19,5 +15,5 @@ setup(name='contextio',
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
-    download_url='https://github.com/contextio/Python-ContextIO/tarball/1.4',
+    download_url='https://github.com/contextio/Python-ContextIO/tarball/1.5',
 )
